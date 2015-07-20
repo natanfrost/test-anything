ba1b9c6130447a7b3d384bf7fc14e003c6b521bd
natanfrost <nataniel.r.v@gmail.com>
2015-07-20 10:29:15 -0300

    bye

--- last_test.js
+++ last_test.js
@@ -1,1 +1,10 @@
 var feedCat = require(process.argv[2])
+var test = require('tape')
+
+test('no chocolate to cats', function (t) {
+  t.plan(2)
+  t.equal(feedCat('food'), 'yum')
+  t.throws(function (){
+    feedCat('chocolate')
+  })
+})
